c9c783ea7eda8bd5c36b2e7f63e41e152a928984
svillemez <65796128+svillemez@users.noreply.github.com>
2020-07-20 21:20:48 -0500

    import os and csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,11 @@
 # main.py for PyBank
+
+# Import the os module
+
+import os
+
+# Import the module for reading CSV files
+
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,11 @@
 # main.py for PyPoll
+
+# Import the os module
+
+import os
+
+# Import the module for reading CSV files
+
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
