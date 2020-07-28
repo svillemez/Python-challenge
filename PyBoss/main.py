b8fca62aca9d380c783ce55e061d1e34a9b2a6f3
svillemez <65796128+svillemez@users.noreply.github.com>
2020-07-28 18:14:17 -0500

    PyBoss

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -10,6 +10,66 @@ import csv
 
 csvpath = os.path.join('Resources','employee_data.csv')
 
+#Create state abbreviation dictionary
+us_state_cd = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
 # Read CSV using CSV module
 
 with open(csvpath) as csvfile:
@@ -30,32 +90,24 @@ with open(csvpath) as csvfile:
 # Read each row of data from CSV file and tally votes
     for row in csvreader:
         emp_id = str(row[0])
-        first_name = str(row[1])
-        last_name = str(row[1])
-        dob = str(row[2]
-        ssn = str("***-**-") + str(row[3])
-        state = 
+        name = str(row[1])
+        first_name = name[:name.index(" ")]
+        last_name = name[name.index(" "):]
+# convert DOB YYYY-MM-DD to MM/DD/YYYY
+        dob = str(row[2])
+        dob_year = (dob[4:])
+        dob_day = (dob[-2:])
+        dob_mo = (dob[-5:])
+        dob_converted = dob_mo + "/" + dob_day + "/" + dob_year
+# convert SSN to hide all but last 4 digits
+        ssn_in = str(row[3])
+        last4 = ssn_in[-4:]
+        ssn = str("***") + str("-") + str("**") + str("-") + str(last4)
+        state = str(row[4])
+        state_cd = str(us_state_cd[state])
+# write converted emplyee file
+        f.write(f"{emp_id},{first_name},{last_name},{dob_converted},{ssn},{state_cd}")
+        f.write("\n")
+# Close Converted CSV File
 
-        f.write(f{emp_id},{first_name},{last+name},
-
-# Print Report Header and Total Votes to Terminal
- 
-
-# Write Report Header and Total Votes to Text File
-
-# Move to next row
-f.write("\n")
-
-
-# Print Election Results to Terminal and Write Election Results to Text File
-
-for x in sorted_candidate_data:
-    name = str(x)
-    votes = int(sorted_candidate_data[x])
-    percent_votes = round((votes/total_votes)*100)
-    f.write(f"{name}: {percent_votes}.000% ({votes:,})")
-    f.write("\n")
-
- # Close Converted CSV File
-
-f.close()
+    f.close()
